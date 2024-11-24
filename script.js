139aa4b66e302ff321a506a126953a7c62b99297
Mike <mg.korop@gmail.com>
2024-11-24 12:15:02 +0200

    fixed slider, refactored code

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//  взяти посилання на кнопки 
-// взяти посилання на список фотографій 
-// створити функцію для роботи слайдера 
-//  рефакторинг 
-
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const listOfImages = document.querySelector(".slides");
@@ -11,27 +6,17 @@ let currentSlide = 0;
 
 function moveSlides(position) {
 	const totalSlides = listOfImages.children.length;
-	
-	if (position >= totalSlides) {
-		position = 0;
-	} else if (position < 0) {
-		position = totalSlides - 1;
-	} 
+
+	if (position < 0) {
+		currentSlide = position = totalSlides - 1;
+	}
+	if (position > totalSlides - 1) {
+		currentSlide = position = 0;
+	}
 	
 	listOfImages.style.transform = `translateX(-${position * 800}px)`;
 }
 
 
-prevBtn.addEventListener('click',()=> moveSlides(currentSlide -1));
-nextBtn.addEventListener('click',()=> moveSlides(currentSlide+1));
-
-
-
-
-
-
-
-
-
-
-
+prevBtn.addEventListener('click', () => {currentSlide -= 1; moveSlides(currentSlide)});
+nextBtn.addEventListener('click', () => {currentSlide += 1; moveSlides(currentSlide)});
